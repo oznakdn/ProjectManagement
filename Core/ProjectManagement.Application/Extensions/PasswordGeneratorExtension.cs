accc7e1729e195550cee1cc068ae60a5701caf5a
ozan lütfü akaydın <ozanakaydin@gmail.com>
2022-10-21 19:26:12 +0300

    For Salting and Hashing created PasswordGenerator extension

---
+++ Core/ProjectManagement.Application/Extensions/PasswordGeneratorExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using NETCore.Encrypt.Extensions;
+
+namespace ProjectManagement.Application.Extensions
+{
+    public class PasswordGeneratorExtension
+    {
+        public static string PasswordHashGenerate(IConfiguration configuration,string password)
+        {
+            string salt = configuration.GetValue<string>("Password:Salt");
+            string salted = $"{password}{salt}";
+            string hashed = salted.MD5();
+            return hashed;
+
+        }
+    }
+}
