c6dbfdff807095a134c3ece472048d82ea6c096a
ozan lütfü akaydın <ozanakaydin@gmail.com>
2022-11-04 11:07:57 +0300

    Some changes

--- Core/ProjectManagement.Domain/Entities/Project.cs
+++ Core/ProjectManagement.Domain/Entities/Project.cs
@@ -8,10 +8,22 @@
             Employees = new HashSet<Employee>();
         }
 
+
         public string ProjectTitle { get; set; }
         public string ProjectDetails { get; set; }
         public DateTime ProjectStartDate { get; set; }
         public DateTime ProjectEndDate { get; set; }
+
+        // TODO : burası düzenlenecek
+        //private DateTime _projectEndDate;
+        //public DateTime ProjectEndDate 
+        //{
+        //    get => _projectEndDate;
+        //    set
+        //    {
+        //       _projectEndDate= ProjectStartDate > ProjectEndDate ? throw new Exception("Project start date must be less then project end date!"): value;
+        //    }
+        //}
 
 
         public virtual ICollection<Employee> Employees { get; set; }
