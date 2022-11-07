6a298a801b9b32b70fedb440b7054ee4e8853d74
ozan lütfü akaydın <ozanakaydin@gmail.com>
2022-11-07 09:10:07 +0300

    _Layout.cshtml updated

--- Client/ProjectManagement.WebMvc/ProjectManagement.WebMvc/Program.cs
+++ Client/ProjectManagement.WebMvc/ProjectManagement.WebMvc/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
