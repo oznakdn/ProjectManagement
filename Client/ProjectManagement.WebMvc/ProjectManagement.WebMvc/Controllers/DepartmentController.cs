233909afa08c188610a544b7a0fd0f8f4afc9afe
ozan lütfü akaydın <ozanakaydin@gmail.com>
2022-11-11 12:57:17 +0300

    Department controller's update, delete actions and views were created.

--- Client/ProjectManagement.WebMvc/ProjectManagement.WebMvc/Controllers/DepartmentController.cs
+++ Client/ProjectManagement.WebMvc/ProjectManagement.WebMvc/Controllers/DepartmentController.cs
@@ -1,26 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NuGet.Packaging;
 using ProjectManagement.WebMvc.Helpers;
 using ProjectManagement.WebMvc.Models.DepartmentViewModels;
+using System.Net.Http.Json;
 
 namespace ProjectManagement.WebMvc.Controllers
 {
     public class DepartmentController : Controller
     {
-       
+
         public async Task<IActionResult> Index()
         {
-           
+
             //var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             //client.DefaultRequestHeaders.Accept.Add(contentType);
 
             //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("accessToken"));
 
-            var client =TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
+            var client = TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
 
-            HttpResponseMessage response =await client.GetAsync("https://localhost:7122/api/Departments");
-            string stringData =await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7122/api/Departments");
+            string stringData = await response.Content.ReadAsStringAsync();
 
 
             List<GetDepartmentsViewModel> departments = JsonConvert.DeserializeObject<List<GetDepartmentsViewModel>>(stringData);
@@ -29,8 +31,16 @@ namespace ProjectManagement.WebMvc.Controllers
                 ViewBag.message = "Unauthorized Person";
                 return PartialView("_UnauthorizePartialView");
             }
-                
+
             return View(departments);
+        }
+
+        public async Task<IActionResult>Details(string departmentName)
+        {
+            var client = TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
+            var response = await client.GetFromJsonAsync<GetDepartmentsViewModel>($"https://localhost:7122/api/Departments/{departmentName}");
+
+            return View(response);
         }
 
 
@@ -48,7 +58,7 @@ namespace ProjectManagement.WebMvc.Controllers
                 var client = TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
                 response = await client.PostAsJsonAsync("https://localhost:7122/api/Departments", createDepartment);
 
-                if(response.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
                     return RedirectToAction(nameof(Index));
                 }
@@ -56,6 +66,41 @@ namespace ProjectManagement.WebMvc.Controllers
             }
 
             return View(createDepartment);
+        }
+
+      
+        public async Task<IActionResult> Edit(string departmentName)
+        {
+            var client = TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
+            var response =await client.GetFromJsonAsync<UpdateDepartmentViewModel>($"https://localhost:7122/api/Departments/{departmentName}");
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateDepartmentViewModel updateDepartment)
+        {
+            var client = TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
+            var response = await client.PutAsJsonAsync("https://localhost:7122/api/Departments", updateDepartment);
+            if(ModelState.IsValid)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(updateDepartment);
+
+            }
+
+            return View(updateDepartment);
+
+
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var client = TokenSavingSessionHelper.TokenSaveOnSession(HttpContext);
+            var response = await client.DeleteAsync($"https://localhost:7122/api/Departments/{id}");
+            return RedirectToAction(nameof(Index));
         }
     }
 
---
+++ Client/ProjectManagement.WebMvc/ProjectManagement.WebMvc/Models/DepartmentViewModels/UpdateDepartmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagement.WebMvc.Models.DepartmentViewModels
+{
+    public class UpdateDepartmentViewModel
+    {
+
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [MinLength(2)]
+        public string DepartmentName { get; set; }
+    }
+}
